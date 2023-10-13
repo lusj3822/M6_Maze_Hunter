ebe6a2df8c21cff1e75ca40192c025e3c5d95322
Mauritz <mauritz.sjodin3@gmail.com>
2023-10-13 10:25:16 +0200

    skapat labyrint som man kan gå runt i, och man kan inte gå igenom väggar.

--- main.py
+++ main.py
@@ -1,47 +1,95 @@
-# Example file showing a circle moving on screen
 import pygame
+from enum import Enum
 
-# pygame setup
+class Direction(Enum):
+    UP = 0
+    DOWN = 1
+    LEFT = 2
+    RIGHT = 3
+
+PLAYER_RADIUS = 10
+MOVE_AMOUNT = 300
+
+def is_black(color):
+        return color.r == 0 and color.g == 0 and color.b == 0
+
+def get_color_at(x, y):
+    return screen.get_at([int(x), int(y)])
+
+def get_next_pos(current_pos, direction):
+    x = current_pos.x
+    y = current_pos.y
+    if direction == Direction.UP:
+        y -= MOVE_AMOUNT * dt
+    elif direction == Direction.DOWN:
+        y += MOVE_AMOUNT * dt
+    elif direction == Direction.LEFT:
+        x -= MOVE_AMOUNT * dt
+    if direction == Direction.RIGHT:
+        x += MOVE_AMOUNT * dt
+    return (x, y)
+
+def is_valid_movement(current_pos, direction):
+    x, y = get_next_pos(current_pos, direction)
+
+    up_color = get_color_at(x, y - PLAYER_RADIUS)
+    down_color = get_color_at(x, y + PLAYER_RADIUS)
+    right_color = get_color_at(x + PLAYER_RADIUS, y)
+    left_color = get_color_at(x - PLAYER_RADIUS, y)
+    if (is_black(up_color) or
+            is_black(down_color) or
+            is_black(left_color) or
+            is_black(right_color)):
+        return False
+
+    return True
+
+def move(player_pos, direction):
+    if is_valid_movement(player_pos, direction) == False:
+        return
+
+    if direction == Direction.UP:
+        player_pos.y -= MOVE_AMOUNT * dt
+    elif direction == Direction.DOWN:
+        player_pos.y += MOVE_AMOUNT * dt
+    elif direction == Direction.LEFT:
+        player_pos.x -= MOVE_AMOUNT * dt
+    elif direction == Direction.RIGHT:
+        player_pos.x += MOVE_AMOUNT * dt
+
+
+
+
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+width = 1280
+height = 720
+screen = pygame.display.set_mode((width, height))
 clock = pygame.time.Clock()
-running = True
+maze = pygame.image.load("maze_1.png").convert()
+maze = pygame.transform.scale(maze, (width, height))
+player_pos = pygame.Vector2(screen.get_width() / 2 - 50, screen.get_height() / 2)
+
 dt = 0
-
-player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
-
+running = True
 while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
-
-    pygame.draw.circle(screen, "red", player_pos, 40)
+    screen.blit(maze, (0, 0))
+    pygame.draw.circle(screen, "red", player_pos, PLAYER_RADIUS)
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
-        player_pos.y -= 300 * dt
+        move(player_pos, Direction.UP)
     if keys[pygame.K_s]:
-        player_pos.y += 300 * dt
+        move(player_pos, Direction.DOWN)
     if keys[pygame.K_a]:
-        player_pos.x -= 300 * dt
+        move(player_pos, Direction.LEFT)
     if keys[pygame.K_d]:
-        player_pos.x += 300 * dt
+        move(player_pos, Direction.RIGHT)
 
-    # flip() the display to put your work on screen
     pygame.display.flip()
-
-    # limits FPS to 60
-    # dt is delta time in seconds since last frame, used for framerate-
-    # independent physics.
     dt = clock.tick(60) / 1000
 
 pygame.quit()
-
-#### i was here
-def jens(dawd):
-    return dawd
